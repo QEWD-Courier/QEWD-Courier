3d835e6cf4286d3f4da2c8f58cf1edd7dbd7cd37
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2019-03-16 15:10:20 +0300

    update author

--- helm/openehr_service/spec/helpers/utils.js
+++ helm/openehr_service/spec/helpers/utils.js
@@ -8,7 +8,7 @@
  | http://rippleosi.org                                                     |
  | Email: code.custodian@rippleosi.org                                      |
  |                                                                          |
- | Author: Rob Tweed, M/Gateway Developments Ltd                            |
+ | Author: Alexey Kucherenko <alexei.kucherenko@gmail.com>                  |
  |                                                                          |
  | Licensed under the Apache License, Version 2.0 (the "License");          |
  | you may not use this file except in compliance with the License.         |
@@ -23,7 +23,7 @@
  |  limitations under the License.                                          |
  ----------------------------------------------------------------------------
 
-  1 March 2019
+  16 March 2019
 
 */
 
@@ -33,17 +33,6 @@ exports.uuidV4Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}
 
 exports.clone = function (obj) {
   return JSON.parse(JSON.stringify(obj));
-};
-
-exports.__revert__ = function (obj) {
-  obj.__revert__();
-  delete obj.__revert__;
-};
-
-exports.rfc3986 = function (str) {
-  return str.replace(/[!'()*]/g, function (c) {
-    return '%' + c.charCodeAt(0).toString(16).toUpperCase();
-  });
 };
 
 exports.isUuidV4 = function (s) {
--- helm/openehr_service/spec/mocks/worker.js
+++ helm/openehr_service/spec/mocks/worker.js
@@ -8,7 +8,7 @@
  | http://rippleosi.org                                                     |
  | Email: code.custodian@rippleosi.org                                      |
  |                                                                          |
- | Author: Rob Tweed, M/Gateway Developments Ltd                            |
+ | Author: Alexey Kucherenko <alexei.kucherenko@gmail.com>                  |
  |                                                                          |
  | Licensed under the Apache License, Version 2.0 (the "License");          |
  | you may not use this file except in compliance with the License.         |
@@ -23,7 +23,7 @@
  |  limitations under the License.                                          |
  ----------------------------------------------------------------------------
 
-  3 March 2019
+  16 March 2019
 
 */
 
@@ -57,7 +57,4 @@ module.exports = function () {
   };
 
   this.qewdSessionByJWT = jasmine.createSpy();
-  this.jwt.handlers = {
-    validateRestRequest: jasmine.createSpy()
-  };
 };
