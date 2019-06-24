5e251b8e12e1184f6d27bf1646005706348676a7
Dmitry Solyannik <dmitry.solyannik@gmail.com>
2019-06-24 18:20:24 +0300

    Changed role to patient and nhsNumber for patient ivor.cox

--- main/auth_service/apis/oidc_callback/index.js
+++ main/auth_service/apis/oidc_callback/index.js
@@ -90,6 +90,10 @@ module.exports = function(args, finished) {
 
       // possibly use verify_jwt.sub as a key for a global or session record
       //  could use session and give it the same timeout as jwt
+      if(verify_jwt.email === 'ivor.cox@gmail.com') {
+          session.role = 'phrUser';
+          session.nhsNumber = 9999990000;
+      }
 
       finished({
         ok: true,
